95f446d4af02276b905ca46234ec710bdd44ef8e
Matthias Pfefferle <pfefferle@gmail.com>
2018-01-30 23:52:56 +0100

    fixed base64 encoding

--- includes/functions.php
+++ includes/functions.php
@@ -1,27 +1,12 @@
 <?php
 if ( ! function_exists( 'base64url_encode' ) ) {
 	function base64_url_encode( $input, $nopad = 1, $wrap = 1 ) {
-		$data = base64_encode( $input );
-		if ( $nopad ) {
-			$data = str_replace( '=' , '' , $data );
-		}
-		$data = strtr( $data, '+/=', '-_,' );
-		if ( $wrap ) {
-			$datalb = '';
-			while ( strlen( $data ) > 64 ) {
-				$datalb .= substr( $data, 0, 64 ) . PHP_EOL;
-				$data = substr( $data, 64 );
-			}
-			$datalb .= $data;
-			return $datalb;
-		} else {
-			return $data;
-		}
+		return strtr( base64_encode( $input ), '+/', '-_' );
 	}
 }
 
 if ( ! function_exists( 'base64url_decode' ) ) {
 	function base64_url_decode( $input ) {
-		return base64_decode( strtr( $input, '-_,', '+/=' ) );
+		return base64_decode( strtr( $input, '-_', '+/' ) );
 	}
 }
