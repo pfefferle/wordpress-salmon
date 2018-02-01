d81f5bb4ede9069d17ebdec4af159d9b20a3ba71
Matthias Pfefferle <pfefferle@gmail.com>
2018-02-01 23:10:43 +0100

    added PHP doc
    and unused function params

--- includes/functions.php
+++ includes/functions.php
@@ -1,11 +1,25 @@
 <?php
 if ( ! function_exists( 'base64_url_encode' ) ) {
-	function base64_url_encode( $input, $nopad = 1, $wrap = 1 ) {
+	/**
+	 * Encode data
+	 *
+	 * @param string $input input text
+	 *
+	 * @return string the encoded text
+	 */
+	function base64_url_encode( $input ) {
 		return strtr( base64_encode( $input ), '+/', '-_' );
 	}
 }
 
 if ( ! function_exists( 'base64_url_decode' ) ) {
+	/**
+	 * Dencode data
+	 *
+	 * @param string $input input text
+	 *
+	 * @return string the decoded text
+	 */
 	function base64_url_decode( $input ) {
 		return base64_decode( strtr( $input, '-_', '+/' ) );
 	}
